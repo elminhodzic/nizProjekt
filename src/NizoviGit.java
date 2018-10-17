f9c0ed33140279f3ec5f3b9d26f1439e38db6765
Eldar Mujezinovic <emujezinovic99@gmail.com>
2018-10-17 17:42:02 +0200

    nizovi

--- src/NizoviGit.java
+++ src/NizoviGit.java
@@ -1,4 +1,29 @@
 
 public class NizoviGit {
+    
+    public double najmanjiBrojUNizu (double [] niz) {
+	return 0;
+    }
+    
+    public double najveciBrojUNizu (double [] niz) {
+	return 0;
+    }
+    
+    public double zbirSvihBrojevaUNizu (double [] niz) {
+	return 0;
+    }
+    
+    public double prosjekSvihBrojevaUNizu (double [] niz) {
+	return 0;
+    }
+    
+    public double sveUnikatneBrojeveUNizu (double [] niz) {
+	return 0;
+    }
+    
+    public double [] sortiraniOdManjegKaVecem (double [] niz) {
+	return niz;
+	
+    }
 
 }
