70eab7a80fe02b28445a6d9babadda76f3fd0ce9
Elmin Hodzic <elmin.hodzic@hotmail.com>
2018-10-19 05:34:05 +0200

    metoda zbir niza, medota prosjek niza, metoda sortirani niz i main metoda implementirana sa testnom funkcijom

--- src/NizoviGit.java
+++ src/NizoviGit.java
@@ -1,7 +1,41 @@
+import java.util.Scanner;
 
 public class NizoviGit {
 
-	public double najmanjiBrojUNizu(double[] niz) {
+	public static void main(String[] args) {
+
+		Scanner unos = new Scanner(System.in);
+		System.out.println("unesi niz duzine 5");
+		// unos.close();
+		double[] niz = new double[5];
+
+		for (int i = 0; i < niz.length; i++) {
+
+			niz[i] = unos.nextDouble();
+		}
+		System.out.println("najmanji je");
+		System.out.println(najmanjiBrojUNizu(niz));
+		System.out.println();
+		System.out.println("najveci je");
+		System.out.println(najveciBrojUNizu(niz));
+		System.out.println();
+		System.out.println("zbir svih brojeva");
+		System.out.println(zbirSvihBrojevaUNizu(niz));
+		System.out.println();
+		System.out.println("prosjek");
+		System.out.println(prosjekSvihBrojevaUNizu(niz));
+		System.out.println();
+		System.out.println("sortirani");
+		sortiraniOdManjegKaVecem(niz);
+
+		for (int i = 0; i < niz.length; i++) {
+
+			System.out.print(niz[i] + " ");
+		}
+
+	}
+
+	public static double najmanjiBrojUNizu(double[] niz) {
 		double min = niz[0];
 		for (int i = 1; i < niz.length; i++) {
 			if (niz[i] < min)
@@ -10,30 +44,60 @@ public class NizoviGit {
 		return min;
 	}
 
-	public double najveciBrojUNizu(double[] niz) {
+	public static double najveciBrojUNizu(double[] niz) {
 		double max = niz[0];
 		for (int i = 1; i < niz.length; i++) {
-			if (niz[i] < max)
+			if (niz[i] > max)
 				max = niz[i];
 		}
 		return max;
 	}
 
-	public double zbirSvihBrojevaUNizu(double[] niz) {
-		return 0;
+	public static double zbirSvihBrojevaUNizu(double[] niz) {
+
+		double zbir = 0;
+
+		for (int i = 0; i < niz.length; i++) {
+
+			zbir += niz[i];
+		}
+
+		return zbir;
 	}
 
-	public double prosjekSvihBrojevaUNizu(double[] niz) {
-		return 0;
+	public static double prosjekSvihBrojevaUNizu(double[] niz) {
+
+		double zbir = 0, prosjek = 0;
+
+		for (int i = 0; i < niz.length; i++) {
+
+			zbir += niz[i];
+		}
+
+		prosjek = zbir / niz.length;
+
+		return prosjek;
 	}
 
-	public double[] sveUnikatneBrojeveUNizu(double[] niz) {
+	public static double[] sveUnikatneBrojeveUNizu(double[] niz) {
 		return niz;
 	}
 
-	public double[] sortiraniOdManjegKaVecem(double[] niz) {
-		return niz;
+	public static double[] sortiraniOdManjegKaVecem(double[] niz) {
 
+		double[] broj = new double[niz.length];
+		double n, temp;
+
+		for (int i = 0; i < niz.length; i++) {
+			for (int j = i + 1; j < niz.length; j++) {
+				if (niz[i] > niz[j]) {
+					temp = niz[i];
+					niz[i] = niz[j];
+					niz[j] = temp;
+				}
+			}
+		}
+		return broj;
 	}
 
 }
